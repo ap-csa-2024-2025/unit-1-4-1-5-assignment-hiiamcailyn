63594f565797dfd659f21052a49b7de2eafc90b0
hiiamcailyn <cailyntran@gmail.com>
2024-09-11 10:10:18 -0700

    hi

--- Problem0.java
+++ Problem0.java
@@ -3,6 +3,8 @@ public class Problem0
   public static void main(String[] args)
   {
     double someVar = 3;
-    System.out.print(someVar);
+    int ar = 3.0; 
+    System.out.print(someVar); // cannot run
+    System.out.print (ar); //cannot run
   }
 }
--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,16 @@
+public class Problem1
+{
+    public static void main(String[] args );
+    {
+        int x = 24;
+        x++; //25
+        x++;  //26
+        x++; //27
+        x++; //28
+        x--; //27
+        x--; //26
+        x--;//25
+        x--; //24
+        }
+ }
 
