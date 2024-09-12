cd1e72c6ae18083b343bb6c05d9f57885f0aa7be
hiiamcailyn <cailyntran@gmail.com>
2024-09-12 09:47:34 -0700

    hi

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,13 @@
-
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        int number = 678;
+        System.out.println(number % 10);
+        number /=10;
+        System.out.println(number % 10);
+        number /=10;
+        System.out.println(number % 10);
+        number /=10;
+    }
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,20 @@
+public class Problem3
+{
+    public static void main (String[] args)
+    {
+        int number 5678;
+        int digit1 = (number % 10);
+        number /=10;
+        int digit2 = (number % 10);
+        number /=10;
+        int digit3 = (number % 10);
+        number /=10;
+        int digit4 = (number % 10);
+        number /=10;
+        System.out.println(digit4);
+        System.out.println(digit3);
+        System.out.println(digit2);
+        System.out.println(digit1);
+    }
+}
 
